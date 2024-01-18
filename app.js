04a440768afc806b0c760a012876938a4e55c800
Maxim Dang <70151733+tendze@users.noreply.github.com>
2024-01-18 16:31:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,129 +1,208 @@
+import "https://telegram.org/js/telegram-web-app.js";
+
 var isFirstTime = localStorage.getItem('isFirstTime');
     
-    if (isFirstTime === null || isFirstTime === "true") {
-        // Set flag to false after the first visit
-        localStorage.setItem('isFirstTime', 'false');
-        isFirstTime = true; // Add this line
+if (isFirstTime === null || isFirstTime === "true") {
+    // Set flag to false after the first visit
+    localStorage.setItem('isFirstTime', 'false');
+    isFirstTime = true; // Add this line
+}
+
+function sendDataToTg() {
+    let tg = window.Telegram.WebApp;
+    var name = document.getElementById("title-input").value
+}
+
+function submit() {
+    var questionsCount = document.querySelectorAll('.question').length;
+
+    if (questionsCount > 0 && validateQuestion()) {
+        // You can add logic here to collect and process the user's answers
+        alert("Отправлено!");
+    } else if (questionsCount === 0) {
+        alert("Пожалуйста, добавьте хотя бы один вопрос");
+    } else if (!validateName()) {
+        alert("Введите название квиза")
+    } else {
+        alert("Не у всех вопросов отмечены варианты ответов");
+    } 
+    sendDataToTg()
+}
+
+function toggleAddQuestionForm() {
+    var addQuestionForm = document.getElementById("add-question-form");
+    var addQuestionButton = document.getElementById("add-question-button");
+
+    if (isFirstTime) {
+        addQuestionForm.style.display = "none";
+        isFirstTime = false;
+    } else {
+        addQuestionForm.style.display = (addQuestionForm.style.display === "none") ? "block" : "none";
     }
 
-    function submitQuiz() {
-        var questionsCount = document.querySelectorAll('.question').length;
-        
-        if (questionsCount > 0 && validateQuiz()) {
-            // You can add logic here to collect and process the user's answers
-            alert("Quiz submitted!");
-        } else if (questionsCount === 0) {
-            alert("Please add at least one question before submitting the quiz.");
-        } else {
-            alert("Not all questions have the correct answer selected, or there are invalid questions.");
+    // Toggle button text
+    addQuestionButton.innerText = (addQuestionButton.innerText === "Добавить новый вопрос") ? "Отменить добавление нового вопроса" : "Добавить новый вопрос";
+}
+
+function escapeRegExp(strToEscape) {
+    return strToEscape.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+};
+
+function trimChar(origString, charToTrim) {
+    charToTrim = escapeRegExp(charToTrim);
+    var regEx = new RegExp("^[" + charToTrim + "]+|[" + charToTrim + "]+$", "g");
+    return origString.replace(regEx, "");
+}
+
+function onSwitchToggleChange(checkbox) {
+    var questionBox = checkbox.closest('.question');
+    var checkboxes = questionBox.querySelectorAll('.options input[type="checkbox"]');
+
+    var checkedCheckboxes = 0;
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            checkedCheckboxes++;
         }
+    });
+    
+    if (checkedCheckboxes <= 1) {
+        alert("Можно переключить, если выбрано больше 1 верного ответа")
+        checkbox.checked = false; // Switch off if 1 or fewer checkboxes are checked
     }
+}
 
-    function toggleAddQuestionForm() {
-        var addQuestionForm = document.getElementById("add-question-form");
-        var addQuestionButton = document.getElementById("add-question-button");
+function onCheckboxClick(checkbox) {
+    var questionBox = checkbox.closest('.question');
+    var switchToggle = questionBox.querySelector('.question-checkbox');
+    var checkboxes = questionBox.querySelectorAll('.options input[type="checkbox"]');
 
-        if (isFirstTime) {
-            addQuestionForm.style.display = "none";
-            isFirstTime = false;
-        } else {
-            addQuestionForm.style.display = (addQuestionForm.style.display === "none") ? "block" : "none";
+    var checkedCheckboxes = 0;
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            checkedCheckboxes++;
         }
+    });
+    
+    if (checkedCheckboxes <= 1) {
+        switchToggle.checked = false
+    }
+}
 
-        // Toggle button text
-        addQuestionButton.innerText = (addQuestionButton.innerText === "Add New Question") ? "Cancel Adding New Question" : "Add New Question";
+function addNewQuestion() {
+    var currentQuestionsCount = document.querySelectorAll('.question').length;
+
+    // Check if the limit of 30 questions is reached
+    if (currentQuestionsCount >= 30) {
+        alert("Вы достигли лимита в 30 вопросов");
+        return;
     }
 
-    function addNewQuestion() {
-        var questionContainer = document.getElementById("quiz-container");
-        var newQuestion = document.createElement("div");
+    var questionContainer = document.getElementById("container");
+    var newQuestion = document.createElement("div");
+    
+    var questionText = document.getElementById("new-question").value.trim();
+    var optionsText = trimChar(document.getElementById("new-options").value, ";").split(';');
 
-        var questionText = document.getElementById("new-question").value.trim();
-        var optionsText = document.getElementById("new-options").value.split(',');
+    // Check if question is non-empty and does not exceed 100 characters
+    if (questionText === "" || questionText.length > 100) {
+        alert("Введите текст вопроса ненулевой длины, не превосходящий 100 символов");
+        return;
+    }
 
-        // Check if question is non-empty and does not exceed 100 characters
-        if (questionText === "" || questionText.length > 100) {
-            alert("Please enter a non-empty question with at most 100 characters.");
+    // Check if there are at least 2 options
+    if (optionsText.length < 2 || optionsText.length > 4) {
+        alert("Введите, пожалуйста, хотя бы 2 варианта ответов, но не более 4х");
+        return;
+    }
+
+    // Check if each option does not exceed 100 characters
+    for (var i = 0; i < optionsText.length; i++) {
+        // Check if each variant does not exceed 30 characters
+        if (optionsText[i].trim().length > 30) {
+            alert("каждый вариант ответа не должен превышать 30 символов");
             return;
         }
-
-        // Check if there are at least 2 options
-        if (optionsText.length < 2 || optionsText.length > 4) {
-            alert("Please enter at least two and at most four options for the question.");
-            return;
-        }
-
-        // Check if each option does not exceed 100 characters
-        for (var i = 0; i < optionsText.length; i++) {
-            if (optionsText[i].trim().length > 100) {
-                alert("Each option should have at most 100 characters.");
-                return;
-            }
-
-            // Check if each variant does not exceed 30 characters
-            if (optionsText[i].trim().length > 30) {
-                alert("Each variant should have at most 30 characters.");
-                return;
-            }
-        }
-
-        newQuestion.className = "question";
-        newQuestion.innerHTML = "<h2>" + questionText + "</h2>";
-
-        var optionsList = document.createElement("ul");
-        optionsList.className = "options";
-
-        for (var i = 0; i < optionsText.length; i++) {
-            var optionItem = document.createElement("li");
-            optionItem.className = "option";
-            optionItem.innerHTML = "<input type='checkbox' name='q" + (document.querySelectorAll('.question').length + 1) + "' value='" + optionsText[i].trim() + "'>" + optionsText[i].trim();
-            optionsList.appendChild(optionItem);
-        }
-
-        newQuestion.appendChild(optionsList);
-
-        var deleteButton = document.createElement("button");
-        deleteButton.className = "delete-button";
-        deleteButton.innerHTML = "Delete";
-        deleteButton.style.background = '#ff5252'
-        deleteButton.onclick = function() {
-            deleteQuestion(this);
-        };
-
-        newQuestion.appendChild(deleteButton);
-
-        questionContainer.insertBefore(newQuestion, document.getElementById("add-question-form"));
-
-        // Clear the form
-        document.getElementById("new-question").value = "";
-        document.getElementById("new-options").value = "";
-
-        // Hide the form after adding a question
-        toggleAddQuestionForm();
     }
 
-    function deleteQuestion(button) {
-        var question = button.parentNode;
-        question.parentNode.removeChild(question);
+    newQuestion.className = "question";
+    newQuestion.innerHTML = "<h2>" + questionText + "</h2>";
+
+    // new
+
+    // Add text "Consider partial answers"
+    var considerPartialAnswers = document.createElement("p");
+    considerPartialAnswers.textContent = "Учитывать частичные ответы";
+    newQuestion.appendChild(considerPartialAnswers);
+
+    var switchToggle = document.createElement("label");
+    switchToggle.className = "switch-toggle";
+    switchToggle.innerHTML = "<input type='checkbox' class='question-checkbox' onclick='onSwitchToggleChange(this)' unchecked><span class='slider'></span>";
+    newQuestion.appendChild(switchToggle);
+
+
+    var closeButton = document.createElement("button");
+    closeButton.className = "close-button";
+    closeButton.innerHTML = "✖"; // Unicode character for a cross
+    closeButton.onclick = function() {
+        deleteQuestion(this);
+    };
+    newQuestion.appendChild(closeButton);
+    
+    // new
+
+    var optionsList = document.createElement("ul");
+    optionsList.className = "options";
+    
+
+    for (var i = 0; i < optionsText.length; i++) {
+        var optionItem = document.createElement("li");
+        optionItem.className = "option";
+        optionItem.innerHTML = "<input type='checkbox' onclick='onCheckboxClick(this)' name='q" + (document.querySelectorAll('.question').length + 1) + "' value='" + optionsText[i].trim() + "'>" + optionsText[i].trim();
+        optionsList.appendChild(optionItem);
     }
 
-    function validateQuiz() {
-        var questions = document.querySelectorAll('.question');
+    newQuestion.appendChild(optionsList);
+
+    questionContainer.insertBefore(newQuestion, document.getElementById("add-question-form"));
+
+    // Clear the form
+    document.getElementById("new-question").value = "";
+    document.getElementById("new-options").value = "";
+
+    // Hide the form after adding a question
+    toggleAddQuestionForm();
+}
+
+function deleteQuestion(button) {
+    var question = button.parentNode;
+    question.parentNode.removeChild(question);
+}
+
+function validateQuestion() {
+    var questions = document.querySelectorAll('.question');
+    
+    for (var i = 0; i < questions.length; i++) {
+        var options = questions[i].querySelectorAll('input[type="checkbox"]');
+        var answerSelected = false;
         
-        for (var i = 0; i < questions.length; i++) {
-            var options = questions[i].querySelectorAll('input[type="checkbox"]');
-            var answerSelected = false;
-            
-            for (var j = 0; j < options.length; j++) {
-                if (options[j].checked) {
-                    answerSelected = true;
-                    break;
-                }
-            }
-            if (!answerSelected) {
-                return false; // At least one question doesn't have a selected answer
+        for (var j = 1; j < options.length; j++) {
+            if (options[j].checked) {
+                answerSelected = true;
+                break;
             }
         }
-
-        return true; // All questions have a selected answer
+        if (!answerSelected) {
+            return false; // At least one question doesn't have a selected answer
+        }
     }
+
+    return true; // All questions have a selected answer
+}
+
+function validateName() {
+    var name = document.getElementById("title-input").value
+    if (name == "") {
+        return false;
+    }
+    return true;
+}
